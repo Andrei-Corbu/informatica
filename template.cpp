fe971ee2988a5ae92824efd461b0fcef2c30a8b8
Andrei Corbu <corbu.andrei@yahoo.com>
2020-05-04 09:53:06 +0300

    add template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//#include <fstream>
+//#include <cstring>
+//#include <cmath>
+//#include <algorithm>
+using namespace std;
+//ifstream fin("date.in");
+//ofstream fout("date.out");
+const int mxn=1e9;
+int main(){
+	
+	cout<<endl; return 0;
+}
